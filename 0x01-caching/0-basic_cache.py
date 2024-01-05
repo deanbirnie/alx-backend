0edc0432125aea6f4a55f308d7f43e4d30f5f76f
deanbirnie <deanicld@gmail.com>
2024-01-05 21:43:01 +0000

    Task 0 - Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Basic cache implementation
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    BasicCache class that inherits from BaseCaching
+    """
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if key is not None:
+            return self.cache_data.get(key)
+        return None
