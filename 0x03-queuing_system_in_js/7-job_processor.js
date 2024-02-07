2ca8661a500e632f46b151bb027678cbc6d2645b
deanbirnie <deanicdl@gmail.com>
2024-02-07 21:17:55 +0000

    Task 9 - Tracking progress and errors

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,26 @@
+import { createQueue } from 'kue';
+
+const queue = createQueue();
+
+const blacklistedNumbers = ['4153518780', '4153518781'];
+
+const sendNotification = (phoneNumber, message, job, done) => {
+  job.progress(0, 100);
+
+  if (blacklistedNumbers.includes(phoneNumber)) {
+    const errorMessage = `Phone number ${phoneNumber} is blacklisted`;
+    done(new Error(errorMessage));
+  } else {
+    job.progress(50, 100);
+
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+
+    done();
+  }
+};
+
+queue.process('push_notification_code_2', (job, done) => {
+  const { phoneNumber, message } = job.data;
+
+  sendNotification(phoneNumber, message, job, done);
+});
